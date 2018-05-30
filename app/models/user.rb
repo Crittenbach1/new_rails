fb0e8108e91066f881b0e601fcb8809174f02f5d
Cynthia Rittenbach <crittenbach@gmail.com>
2018-05-30 19:05:29 -0400

    created User omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
+
+  self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.id).first_or_create do |user|
+      user.provider = auth.prodider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
 end
