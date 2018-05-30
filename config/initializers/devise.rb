68d815f8e97b67437d5e9dbfdbdaaf05b113adc6
Cynthia Rittenbach <crittenbach@gmail.com>
2018-05-30 19:30:49 -0400

    facebook key and secret

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '91c8bd28bcd041d5c481587156e5d7877e4600f42758c4ef87709486ee21fdc3d73997f6d495b28dc2a1760e6e700e44b5962ebc3fd47833e8ec2126d01d12ab'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -19,6 +19,8 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],token_params: { parse: :json }
+
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
