dbf4d9e6fd1a2c3c0e0c83909739b58149648f77
Cynthia Rittenbach <crittenbach@gmail.com>
2018-05-30 23:28:15 -0400

    added new genres route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'genre#index'
 
+  resource :genre, only: [:index, :new]
+
 end
